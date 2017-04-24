e384efea64fa8a58e5c148f6de768af465bc9cf9
Juan Esteban Sierra Lara <sierra-lara@hotmail.com>
2017-04-23 21:19:16 -0500

    Added validations on console log

--- controller.js
+++ controller.js
@@ -10,17 +10,6 @@ var Ciudad = require("./modelos/ciudad").Ciudad;
 var Liga = require("./modelos/liga").Liga;
 var Equipo = require("./modelos/equipo").Equipo;
 
-var numeros="0123456789";
-
-function isNumber(texto){
-   for(i=0; i<texto.length; i++){
-      if (numeros.indexOf(texto.charAt(i),0)!=-1){
-         return 1;
-      }
-   }
-   return 0;
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -32,27 +21,33 @@ router.get('/jugador', function(req, res, next){
 });
 
 //CRUD jugador
-router.post('/get-data', function(req,res, ext){
+router.post('/get-data', function(req,res, next){
 
 	var buscar = req.body.buscarJ;
 	console.log("Buscar " + buscar);
 
-	if(buscar == ""){
-		Jugador.find().populate("ciudad").then(function(doc) {
-			//console.log(doc)
+	if(buscar == "" || buscar == null){
+		Jugador.find().populate('ciudad').then(function(doc) {
+			console.log("All " + doc)
 			res.render('jugador', {items: doc});
 		});
 	}else{
 		Jugador.findOne({'nombre': buscar}).populate("ciudad").exec(function(err,jugador) {
-			//console.log("Jugador " + jugador);
-			res.render('jugador', {items: jugador});
+			//Ciudad.findOne({'_id': jugador.ciudad._id}).exec(function(err,ciudad){			
+				if(err){
+					console.log(err);
+				}else{
+					res.render('jugador', {items: jugador});
+				}
+			//});
 		});
+
 	}
 });
 
 router.post('/insert', function(req, res, next) {
 
-	var busqueda = req.body.ciudadJ;
+  var busqueda = req.body.ciudadJ;
 
   Ciudad.findOne({'ciudad': busqueda}).exec(function(err,ciudad){
      if(err){
@@ -67,28 +62,11 @@ router.post('/insert', function(req, res, next) {
        		fechaNacimiento: req.body.fechaNacimiento,
           	ciudad:ciudad._id
           }
-          if (isNumber(item.nombre == 1) && item.nombre != "") {
-          	console.log("Nombre invalido");
-          }
-          else{
-          	if (isNumber(item.piernaPreferida)) {
-          		console.log("Pierna no valida");
-          	}
-          	else{
-          		if(isNumber(item.ciudad) == 1 && item.ciudad != ""){
-          			console.log("ciudad invalida");
-          		}
-          		else{
-          			var data = new Jugador(item);
-          			//console.log(item.ciudad);
-          			data.save().then(function(us){
-            		res.redirect("/jugador");
-          			}); //guarda en la bd
-          		}
-          	}
-
-          }
-          
+          var data = new Jugador(item);
+          //console.log(item.ciudad);
+          data.save().then(function(us){
+            res.redirect("/jugador");
+          }); //guarda en la bd
        }else{
          console.log("Ciudad is null");
        }
@@ -108,30 +86,13 @@ router.post('/update', function(req, res, next) {
 			console.log('No se encontró el jugador');
 		}else{
 
-			name= req.body.nombre;
-			leg = req.body.piernaPreferidaU;
-			date = req.body.fechaNacimientoU;
-
-			if (isNumber(name)==1) {
-				console.log("Nombre invalido");
-			}
-			else{
-				doc.nombre = name;
-				if (leg.lowercase() != "derecha" && leg.lowercase() != "izquierda") {
-					console.log("Pierna invalida");
-				}
-				else{
-					doc.piernaPreferida = leg;
-					doc.fechaNacimiento = date;
-					doc.save();
-					
-				}
-			}
-			
+			doc.nombre = req.body.nombre;
+			doc.piernaPreferida = req.body.piernaPreferidaU;
+			doc.fechaNacimiento = req.body.fechaNacimientoU;
+			doc.save();
 		}
-		res.redirect('/jugador');
 	});
-	
+	res.redirect('/jugador');
 });
 
 router.post('/delete', function(req, res, next) {
@@ -149,7 +110,7 @@ router.get('/ciudad', function(req, res, next){
 router.post('/get-dataC', function(req,res, ext){
 
 	var buscar = req.body.buscarC;
-	console.log("Buscar " + buscar);
+	//console.log("Buscar " + buscar);
 
 	if(buscar == ""){
 		Ciudad.find().then(function(doc) {
@@ -171,24 +132,10 @@ router.post('/insertC', function(req, res, next) {
 		depto: req.body.depto,
 		ciudad: req.body.ciudad
 	};
-	if (isNumber(item.pais)==1 && pais != ""){
-		console.log("Pais invalido");
-	}
-	else{
-		if (isNumber(item.depto)==1 && depto != "") {
-			console.log("Departamento invalido");
-		}
-		else{
-			if (isNumber(item.ciudad)==1 && ciudad != "") {
-				console.log("Ciudad invalida");
-			}
-			else{
-				var data = new Ciudad(item);
-				data.save(); //guarda en la bd
-				res.redirect('/ciudad');
-			}
-		}
-	}
+	var data = new Ciudad(item);
+	data.save(); //guarda en la bd
+
+	res.redirect('/ciudad');
 });
 
 router.post('/updateC', function(req, res, next) {
@@ -201,30 +148,10 @@ router.post('/updateC', function(req, res, next) {
 			console.log('No se encontró el jugador');
 		}else{
 
-			country =req.body.paisU;
-			dep = req.body.deptoU; 
-			city = req.body.ciudadU;
-
-			if (isNumber(country)==1){
-				console.log("Pais invalido");
-			}
-			else{
-				if (isNumber(dep)==1) {
-					console.log("Departamento invalido");
-				}
-				else{
-					if (isNumber(city)==1) {
-						console.log("Ciudad invalida");
-					}
-					else{
-
-						doc.Pais = country;
-						doc.depto = dep;
-						doc.ciudad = city;
-						doc.save();
-					}
-				}
-			}
+			doc.Pais = req.body.paisU;
+			doc.depto = req.body.deptoU;
+			doc.ciudad = req.body.ciudadU;
+			doc.save();
 		}
 	});
 	res.redirect('/ciudad');
@@ -268,20 +195,10 @@ router.post('/insertL', function(req, res, next) {
 		pais: req.body.paisL,
 		
 	};
-	if (isNumber(item.nombre)==1 && nombre != "") {
-		console.log("Nombre invalido");
-	}
-	else{
-		if (isNumber(item.pais)==1 && pais != "") {
-			console.log("Pais invalido");
-		}
-		else{
-			var data = new Liga(item);
-			data.save(); //guarda en la bd
-			res.redirect('/liga');
-		}
-	}
+	var data = new Liga(item);
+	data.save(); //guarda en la bd
 
+	res.redirect('/liga');
 });
 
 router.post('/updateL', function(req, res, next) {
@@ -293,24 +210,9 @@ router.post('/updateL', function(req, res, next) {
 			console.log(err)
 		}else{
 
-		name = req.body.nombreUL;
-		country = req.body.paisUL;
-
-		if (isNumber(nombre)==1) {
-			console.log("Nombre invalido");
-		}
-		else{
-			if (isNumber(pais)==1) {
-				console.log("Pais invalido");
-			}
-			else{
-				doc.nombre = req.body.nombreUL;
-				doc.pais = req.body.paisUL;
-				doc.save();
-			}
-		}
-
-		
+		doc.nombre = req.body.nombreUL;
+		doc.pais = req.body.paisUL;
+		doc.save();
 		}	
 	});
 
@@ -337,12 +239,12 @@ router.post('/get-dataE', function(req,res, ext){
 	//console.log("Buscar " + buscar);
 
 	if(buscar == ""){
-		Equipo.find().populate("liga").then(function(doc) {
+		Equipo.find().populate('liga').then(function(doc) {
 			//console.log(doc)
 			res.render('equipo', {items: doc});
 		});
 	}else{
-		Equipo.findOne({'nombre': buscar}).populate("liga").exec(function(err,equipo) {
+		Equipo.find({'nombre': buscar}).populate("liga").exec(function(err,equipo) {
 			//console.log("Equipo " + equipo);
 			res.render('equipo', {items: equipo});
 		});
@@ -366,29 +268,12 @@ router.post('/insertE', function(req, res, next) {
        				estadio: req.body.estadio
           		};
 
-          		if (isNumber(item.nombre)==1 && nombre != "") {
-          			console.log("Nombre invalido");
-          		}
-          		else{
-          			if (isNumber(item.liga)==1 && liga != "") {
-          				console.log("Liga invalida");
-          			}
-          			else{
-          				if (isNumber(item.estadio)==1 && estadio != "") {
-          					console.log("Estadio invalido");
-          				}
-          				else{
-          					var data = new Equipo(item);
-          					data.save().then(function(us){
-            				res.redirect("/equipo");
-          					}); //guarda en la bd
-          				}
-
-          			}
-          		}
           		//console.log(item);
 
-          	
+          	var data = new Equipo(item);
+          	data.save().then(function(us){
+            	res.redirect("/equipo");
+          	}); //guarda en la bd
         }else{
          console.log("Ciudad is null");
        }
@@ -405,33 +290,10 @@ router.post('/updateE', function(req, res, next) {
 		if(err){
 			console.log('No entry found');
 		}else{
-
-		name = req.body.nombreEU;
-		ligue = req.body.ligaU;
-		stad = req.body.estadioU;
-
-		if (isNumber(name)==1 && nombre != "") {
-          			console.log("Nombre invalido");
-          		}
-        else{
-          	if (isNumber(ligue)==1 && liga != "") {
-          		console.log("Liga invalida");
-          	}
-          	else{
-          		if (isNumber(stad)==1 && estadio != "") {
-          					console.log("Estadio invalido");
-          		}
-          		else{
-          			doc.nombre = req.body.nombreEU;
-					doc.liga = req.body.ligaU;
-					doc.estadio = req.body.estadioU;
-					doc.save();
-          		}
-
-          	}
-        }
-
-
+		doc.nombre = req.body.nombreEU;
+		doc.liga = req.body.ligaU;
+		doc.estadio = req.body.estadioU;
+		doc.save();
 		}
 	});
 	res.redirect('/equipo');
